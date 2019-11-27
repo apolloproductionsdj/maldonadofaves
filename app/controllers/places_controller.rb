79c7ffad214b8a7640065d5b2859be03309f0f9b
Mike Torres <apolloproductionsdj@gmail.com>
2019-11-27 15:16:13 +0000

    ran rake assets:precompile to help solve precompiling assests failed message

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -47,9 +47,11 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Alllowed', status: :forbidden
+    end 
+    
     @place.destroy
     redirect_to root_path
-    end
+  
   end
 
   private
